aebb6b681f35db180e57615401cbfd1daaf161d0
sarra <sarra.douiri@hotmail.fr>
2023-02-01 20:07:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './Components/navBar'
+
+
+function App() {
+<div class="container">
+ <div class="row">
+    <div class="col-md-6 left">a</div>
+    <div class="col-md-6 right">a</div>
+ </div>
+</div>
+
+}
+
+export default App
